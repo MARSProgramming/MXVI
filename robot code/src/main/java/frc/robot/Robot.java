2a11e99efbf28de95444b15a37f5f55f83bfad32
Shawn <shmorganl14@gmail.com>
2023-01-28 19:53:45 -0500

    disabled livewindow in testinit

--- robot code/src/main/java/frc/robot/Robot.java
+++ robot code/src/main/java/frc/robot/Robot.java
@@ -108,6 +108,7 @@ public class Robot extends TimedRobot {
   public void testInit() {
     // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
+    LiveWindow.setEnabled(false);
   }
 
   /** This function is called periodically during test mode. */
