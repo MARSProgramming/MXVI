8454cecf1dadc7e394214be3517e4e380d0f54b3
Helen-42 <68658693+Helen-42@users.noreply.github.com>
2023-01-29 12:59:23 -0500

    Update Arm.java
    added a wrist motor to Arm Subsystem

--- robot code/src/main/java/frc/robot/subsystems/Arm.java
+++ robot code/src/main/java/frc/robot/subsystems/Arm.java
@@ -7,20 +7,28 @@ import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import io.github.oblarg.oblog.annotations.Log;
 
 public class Arm extends SubsystemBase{
     private TalonFX mElbow;
     private TalonFX mShoulder;
+    private TalonFX mWrist;
+    private double wristPosition;
+        
     public Arm(){
+
         mElbow = new TalonFX(Constants.Arm.kElbowMotorID);
         mShoulder = new TalonFX(Constants.Arm.kShoulderMotorID);
+        mWrist = new TalonFX(Constants.Arm.kWristMotorID);
 
         TalonFXConfiguration config = new TalonFXConfiguration();
         mElbow.configAllSettings(config);
         mShoulder.configAllSettings(config);
+        mWrist.configAllSettings(config);
 
         mElbow.setNeutralMode(NeutralMode.Brake);
         mShoulder.setNeutralMode(NeutralMode.Brake);
+        mWrist.setNeutralMode(NeutralMode.Brake);
     }
 
     public void runElbowPOutput(double v){
@@ -30,4 +38,13 @@ public class Arm extends SubsystemBase{
     public void runShoulderPOutput(double v){
         mShoulder.set(ControlMode.PercentOutput, v);
     }
-}
+
+    public void runWristPOutput(double v){
+        mWrist.set(ControlMode.PercentOutput, v);
+    }
+
+    @Override
+    public void periodic(){
+       wristPosition = mWrist.getSelectedSensorPosition()* 2*Math.PI/4096;
+    }
+    }
