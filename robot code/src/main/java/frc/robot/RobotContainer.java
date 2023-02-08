a6236c2ebaa2997a1614eeca931a879a67756a66
gander2 <89546715+gander2@users.noreply.github.com>
2023-02-08 18:46:44 -0500

    actual final changes to robot container file
    imported Trigger

--- robot code/src/main/java/frc/robot/RobotContainer.java
+++ robot code/src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.DefaultDriveCommand;
 import frc.robot.commands.ZeroGyroscope;
 import frc.robot.commands.ZeroSwerves;
