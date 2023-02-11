d24b9dd17e1034c8e79f3e2f7d436c75fd4f1f5f
paperclip <66579862+gitpaperclip@users.noreply.github.com>
2023-02-11 16:47:23 -0500

    Update IntakeSubsystem.java

--- robot code/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ robot code/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems;
 
-import java.util.function.DoubleSupplier;
-
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
@@ -10,7 +8,6 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class IntakeSubsystem extends SubsystemBase {
     private DoubleSolenoid mSolenoid = new DoubleSolenoid(61, PneumaticsModuleType.REVPH, 2, 6);
@@ -21,32 +18,28 @@ public class IntakeSubsystem extends SubsystemBase {
         mSolenoid.set(Value.kReverse);
     }
     public void extend(){
-      mSolenoid.set(Value.kForward);
-    }
-    public void retract(){
-      mSolenoid.set(Value.kReverse);
-    }
+        mSolenoid.set(Value.kForward);
+      }
+      public void retract(){
+        mSolenoid.set(Value.kReverse);
+      }
+      public void toggle(){
+        if(mSolenoid.get() == Value.kOff){mSolenoid.set(Value.kReverse);}
+         mSolenoid.toggle();
+      }
+    public void RunMotors(double voltage) {
+      if (Motor0.getStatorCurrent() > 1) {
+        Motor0.set(ControlMode.PercentOutput, 0);
+      } else{
+        Motor0.set(ControlMode.PercentOutput, voltage);
+      }
 
-    public CommandBase toggleIntake() {
-      return runOnce(
-        () -> {
-          if (mSolenoid.get()==Value.kOff)
-            mSolenoid.set(Value.kReverse); 
-          else
-            mSolenoid.toggle();
-        }).withName("Test Intake Pneumatics");
     }
-    public CommandBase runIntakeMotors(DoubleSupplier percent) {
-      return run(() -> Motor0.set(ControlMode.PercentOutput, percent.getAsDouble()))
-        .withName("Test Intake Motor");
-    }
-
-    public void runIntakePOutput(double v){
-      Motor0.set(ControlMode.PercentOutput, v);
-    }
-    
     @Override
     public void periodic() {
       SmartDashboard.putString("IntakeSolenoid State", mSolenoid.get().toString());
+    }
+    public void runIntakePOutput(double d) {
+      
     }
 }
